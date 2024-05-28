483ebef8dead79a75f745bfd345802075fd90bca
hagbazzinfo001 <151969171+hagbazzinfo001@users.noreply.github.com>
2024-05-28 22:42:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// var not=prompt("Do you want to be receiving notification for us")
+//          if (not=true) {
+//             prompt=("enter your email!")
+//          }
+//     else{
+//     alert("you\'ve cancelled this request")
+// }
+// var age=prompt("what is your age")
+//    if (age=== "18"){
+// alert("congration on you 1st year of drivig")
+//    }else if (age>="18"){
+//     alert ("Power on! drive")
+// }
+// else{
+//     alert("you\'re not eliglible to drive")
+// }
+// var not=prompt("Do you want to be receiving notification for us? enter email")
+//           if( not !=null && not !=''){
+//                  alert("Thankssssss")
+//              }
+//     else{
+//     alert("you\'ve cancelled this request")
+//        }
+// var number=10;
+// while(number>9)
+//  {
+//         console.log (number);
+// number++;
+// }
+var fruits = [
+  "apple",
+  "mango",
+  "orange",
+  "pineapple",
+  "pawpaw",
+  "gala",
+  "grape",
+  "banana",
+  "bery",
+  "goood",
+];
+ 
+for (var i = 0; i < fruits.length; i++)
+ {
+       console.log( i);
+ }
+ function theFruits(fruit){
+       console.log(fruit);
+ }
+ fruits.forEach(theFruits)
